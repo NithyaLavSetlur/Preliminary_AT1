1f029d23cb07e4ba1425ce7c29b5f2135d0768c3
Nithya Setlur <nithyasetlur@gmail.com>
2024-03-19 14:26:35 +1100

    Version 0

--- flashcards_app/admin.py
+++
@@ -1,9 +0,0 @@
-# flashcards_app/admin.py
-
-from django.contrib import admin
-from .models import Flashcard
-
-@admin.register(Flashcard)
-class FlashcardAdmin(admin.ModelAdmin):
-    list_display = ['question', 'answer']
-    search_fields = ['question']
--- flashcards_app/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class FlashcardsAppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'flashcards_app'
